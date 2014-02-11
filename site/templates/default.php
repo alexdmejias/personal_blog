ba31525a027418d5be479dd383ed9260b961ea4b
alex mejias <alexdmejias@gmail.com>
2014-02-11 14:40:57 -0500

    added dynamic template system; pagination for next + prev pages

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,11 +2,26 @@
 <?php $body_snippets_path = $_SERVER['DOCUMENT_ROOT']. '/site/snippets/body/'; ?>
 
 <section class="content wrap-narrow clearfix">
-	<?php if(isset($page->template->value) && (file_exists($body_snippets_path . '/tip.php'))): ?>
-		<?php snippet(strtolower('body/'.$page->template->value)); ?>
-	<?php else: ?>
-		<?php snippet(strtolower('body/default')); ?>
-	<?php endif; ?>
+	<h2 class="title"><?php echo html($page->title()) ?></h2>
+	<?php
+		snippet('submenu');
+
+		if (isset($page->template->value) && (file_exists($body_snippets_path . '/tip.php'))) {
+			snippet(strtolower('body/'.$page->template->value));
+		} else {
+			snippet(strtolower('body/default'));
+		}
+
+	?>
+	<nav>
+		<?php if($page->hasPrev()): ?>
+			<a href="<?= $page->prev()->url() ?>">&laquo; Prev: <?= $page->prev()->title(); ?></a>
+		<?php endif; ?>
+		<?php if($page->hasNext()): ?>
+			<a href="<?= $page->next()->url() ?>">Next: <?= $page->next()->title() ?> &raquo; </a>
+		<?php endif; ?>
+	</nav>
+
 
 </section>
 
