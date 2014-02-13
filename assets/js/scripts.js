c94c69a92a4a72e4d2b080176d78035e5294c83c
alex mejias <alexdmejias@gmail.com>
2014-02-13 00:18:09 -0500

    cleaned header styles; added scrolling animation to header

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,76 +1,20 @@
-/*
-Bones Scripts File
-Author: Eddie Machado
+var app = {}
+app.windowWidth = 0;
 
-This file should contain any js scripts you want to add to the site.
-Instead of calling it in the header or throwing it inside wp_head()
-this file will be called automatically in the footer so as not to
-slow the page load.
+$(window).on('load resize', function() {
+    app.windowWidth = $(window).width();
+    console.log(app);
+});
 
-*/
-
-// IE8 ployfill for GetComputed Style (for Responsive Script below)
-if (!window.getComputedStyle) {
-    window.getComputedStyle = function(el, pseudo) {
-        this.el = el;
-        this.getPropertyValue = function(prop) {
-            var re = /(\-([a-z]){1})/g;
-            if (prop == 'float') prop = 'styleFloat';
-            if (re.test(prop)) {
-                prop = prop.replace(re, function () {
-                    return arguments[2].toUpperCase();
-                });
-            }
-            return el.currentStyle[prop] ? el.currentStyle[prop] : null;
+$(document).scroll(function(){
+    if (app.windowWidth > 499) {
+        if ($(window).scrollTop() > 20) {
+            $('header').addClass('scrolled')
+        } else {
+            $('header').removeClass('scrolled')
         }
-        return this;
     }
-}
-
-// as the page loads, call these scripts
-jQuery(document).ready(function($) {
-
-    /*
-    Responsive jQuery is a tricky thing.
-    There's a bunch of different ways to handle
-    it, so be sure to research and find the one
-    that works for you best.
-    */
-    
-    /* getting viewport width */
-    var responsive_viewport = $(window).width();
-    
-    /* if is below 481px */
-    if (responsive_viewport < 481) {
-    
-    } /* end smallest screen */
-    
-    /* if is larger than 481px */
-    if (responsive_viewport > 481) {
-        
-    } /* end larger than 481px */
-    
-    /* if is above or equal to 768px */
-    if (responsive_viewport >= 768) {
-    
-        /* load gravatars */
-        $('.comment img[data-gravatar]').each(function(){
-            $(this).attr('src',$(this).attr('data-gravatar'));
-        });
-        
-    }
-    
-    /* off the bat large screen actions */
-    if (responsive_viewport > 1030) {
-        
-    }
-    
-	
-	// add all your scripts here
-	
- 
-}); /* end of as page load scripts */
-
+});
 
 /*! A fix for the iOS orientationchange zoom bug.
  Script by @scottjehl, rebound by @wilto.
