81b9b4005fcc7185e61ba029cf54bdf530f41075
alex mejias <alexdmejias@gmail.com>
2014-02-20 17:47:58 -0500

    added small snippet to grab random images for the projects page

--- site/snippets/body/projects.php
+++ site/snippets/body/projects.php
@@ -9,8 +9,19 @@
 			$children = $page->children();
 			echo '<div class="projects clearfix">';
 			foreach ($page->children() as $key) {
+				$proj_type = $key->project_type ? ' ' . $key->project_type : '';
+
+				/*************/
+				/* Only for testing */
+				/*************/
+				$sample_topics = array('abstract', 'food', 'sports', 'animals', 'nature');
+				$topic = $sample_topics[rand(0, count($sample_topics) - 1)];
+				/*************/
+				/* Only for testing */
+				/*************/
+
 				echo
-					'<div class="project" style="background-image:url(http://lorempixel.com/200/300/abstract)" ><a href="'. $key->url() .'"> '.
+					'<div class="project'. $proj_type .'" style="background-image:url(http://lorempixel.com/200/300/'. $topic.')" ><a href="'. $key->url() .'"> '.
 					'<h5>'.$key->title.'</h5>'.
 					'<p>'.$key->excerpt.'</p>'.
 					'</a></div>';
