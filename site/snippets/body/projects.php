9da018f5cb64c361a8871e17b5fc92a9d6c12594
alex mejias <alexdmejias@gmail.com>
2014-02-21 21:18:14 -0500

    major update to projects page

--- site/snippets/body/projects.php
+++ site/snippets/body/projects.php
@@ -1,34 +1,43 @@
 <section class="content wrap">
-	<h2 class="title"><?php echo html($page->title()) ?></h2>
-
-	<article>
-	<?php echo kirbytext($page->text()) ?>
-
-	<?php
-		if($page->hasChildren() ) {
+	<h2 class="title">
+		<?php echo html($page->title()) ?>
+		<?php
 			$children = $page->children();
+			foreach ($children as $key) {
+				echo '<a href="#'.strtolower(str_replace(' ', '_', $key->title)).'">'.$key->title.'</a>';
+			}
+		?>
+	</h2>
+	<article>
+		<?php
+
+			echo kirbytext($page->text());
+
 			echo '<div class="projects clearfix">';
-			foreach ($page->children() as $key) {
-				$proj_type = $key->project_type ? ' ' . $key->project_type : '';
+			foreach ($page->children() as $child) {
 
-				/*************/
-				/* Only for testing */
-				/*************/
-				$sample_topics = array('abstract', 'food', 'sports', 'animals', 'nature');
-				$topic = $sample_topics[rand(0, count($sample_topics) - 1)];
-				/*************/
-				/* Only for testing */
-				/*************/
-
-				echo
-					'<div class="project'. $proj_type .'" style="background-image:url(http://lorempixel.com/200/300/'. $topic.')" ><a href="'. $key->url() .'"> '.
-					'<h5>'.$key->title.'</h5>'.
-					'<p>'.$key->excerpt.'</p>'.
-					'</a></div>';
+				echo '<div class="clearfix">';
+				echo '<h3 id="'.strtolower(str_replace(' ', '_', $child->title)).'">'.$child->title.'</h3>';
+				echo kirbytext($child->text());
+				foreach ($child->children() as $grandchild) {
+					/*************/
+					/* Only for testing */
+					/*************/
+					$sample_topics = array('abstract', 'food', 'sports', 'animals', 'nature');
+					$topic = $sample_topics[rand(0, count($sample_topics) - 1)];
+					/*************/
+					/* Only for testing */
+					/*************/
+					echo
+						'<div class="project" style="background-image:url(http://lorempixel.com/200/300/'. $topic.')" ><a href="'. $grandchild->url() .'"> '.
+						'<h5>'.$grandchild->title.'</h5>'.
+						'<p>'.$grandchild->excerpt.'</p>'.
+						'</a></div>';
+				}
+				echo '</div>';
 			}
 			echo '</div>';
-		}
-	?>
+		?>
 
 	</article>
 
