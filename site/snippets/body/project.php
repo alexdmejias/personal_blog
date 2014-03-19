1ab8ba78df14fa46a79223eb0e5340166d81e058
alex mejias <alexdmejias@gmail.com>
2014-03-19 12:30:43 -0400

    projects style + cleanup

--- site/snippets/body/project.php
+++ site/snippets/body/project.php
@@ -1,5 +1,14 @@
 <section class="content wrap-narrow clearfix">
-	<h2 class="title"><?php echo html($page->title()) ?> <span><a class="button italic fz-half" target="_blank" href="<?php echo $page->url; ?>">Visit Project</a></span></h2>
+	<h2 class="title"><?php echo html($page->title()) ?>
+		<span>
+			<a class="button italic fz-half" target="_blank" href="<?php echo $page->url; ?>">Visit Project</a>
+		</span>
+		<?php if($page->github): ?>
+			<span>
+				<a class="button italic fz-half" target="_blank" href="<?php echo $page->github; ?>">Github</a>
+			</span>
+		<?php endif; ?>
+	</h2>
 	<?php snippet('submenu'); ?>
 
 	<article>
