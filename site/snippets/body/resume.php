099cacc2df48c66111b4549fa6ac93e493366d6a
alex mejias <alexdmejias@gmail.com>
2014-03-30 18:45:06 -0400

    more resume changes

--- site/snippets/body/resume.php
+++ site/snippets/body/resume.php
@@ -1,15 +1,21 @@
-<section class="content wrap">
-	<h2 class="title"><?php echo html($page->title()) ?></h2>
+<section class="content wrap-narrow">
+	<h2 class="title"><?php echo html($page->title()) ?>
+		<span>
+			<a class="button fz-half italic" href="assets/alexmejias-resume.pdf">View As PDF</a>
+		</span>
+	</h2>
 
 	<article class="clearfix">
 		<div class="personal_info clearfix">
-			<h3>
-				Alex Mejias<br>
+			<h3>Alex Mejias</h3>
+			<h4>
 				alex@alexdmejias.com<br>
-				(516) 748 - 6801<br>
+				(516) 748 - 6801
+			</h4>
+			<h4>
 				alexdmejias.com<br>
 				github.com/amejias101
-			</h3>
+			</h4>
 		</div>
 		<?php echo kirbytext($page->text()) ?>
 	</article>
